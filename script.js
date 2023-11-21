24832af5159524cd54f865942160ad78ca3baf88
Sira <kongsirisira@gmail.com>
2023-11-21 22:40:15 +0700

    its still a mess

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
 
-sidebar.onmouseover = function() {
-    sidebar.classList.toggle("active");
+  sidebar.onmouseover = function() {
+      sidebar.classList.toggle("open");
+  };
+
+  sidebar.onmouseout = function() {
+      sidebar.classList.toggle("open");
+  };
+
+
+  var video = document.getElementById("myVideo");
+  
+  var PPbtn = document.getElementById("pausePlayBtn");
+  
+  
+  play.onclick = function () {
+    PPbtn.play();
 };
 
-sidebar.onmouseout = function() {
-    sidebar.classList.toggle("active");
+stop.onclick = function () {
+  PPbtn.stop();
 };
+
+
