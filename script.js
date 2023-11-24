e681a8ff39222c1eed29fcdefebb73a4b257f48c
Sira <kongsirisira@gmail.com>
2023-11-25 01:35:55 +0700

    almost done with video page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector(".bx-search");
+
 
 sidebar.onmouseover = function() {
     sidebar.classList.toggle("active");
@@ -20,5 +20,81 @@ function openVideo(videoSource) {
 
   // Show the video popup
   videoPopup.style.display = 'flex';
-  videoPopup.style.width = '90%'
+  videoPopup.style.width = '100%'
 }
+
+document.querySelector('.sidebar').addEventListener('mouseover', function() {
+  document.querySelector('.icon1').style.paddingLeft = "10px";
+
+});
+
+document.querySelector('.sidebar').addEventListener('mouseout', function() {
+  document.querySelector('.icon1').style.paddingLeft = "0px";
+
+});
+
+document.querySelector('.sidebar').addEventListener('mouseover', function() {
+  document.querySelector('.icon2').style.paddingLeft = "10px";
+
+});
+
+document.querySelector('.sidebar').addEventListener('mouseout', function() {
+  document.querySelector('.icon2').style.paddingLeft = "0px";
+
+});
+
+
+
+document.querySelector('.sidebar').addEventListener('mouseover', function() {
+  document.querySelector('.icon3').style.paddingLeft = "10px";
+
+});
+
+
+document.querySelector('.sidebar').addEventListener('mouseout', function() {
+  document.querySelector('.icon3').style.paddingLeft = "0px";
+
+});
+
+
+
+
+
+
+document.querySelector('.name1').addEventListener('mouseover', function() {
+  document.querySelector('.icon1').style.color = "var(--shadow)";
+  document.querySelector('.icon1').style.transform = "rotate(-90deg)";
+});
+
+document.querySelector('.name1').addEventListener('mouseout', function() {
+  document.querySelector('.icon1').style.color = "var(--highlight_color)";
+  document.querySelector('.icon1').style.transform = "rotate(0deg)";
+
+});
+
+document.querySelector('.name2').addEventListener('mouseover', function() {
+  document.querySelector('.icon2').style.color = "var(--shadow)";
+
+});
+
+document.querySelector('.name2').addEventListener('mouseout', function() {
+  document.querySelector('.icon2').style.color = "var(--highlight_color)";
+
+});
+
+
+document.querySelector('.name3').addEventListener('mouseover', function() {
+  document.querySelector('.icon3').style.color = "var(--shadow)";
+
+});
+
+document.querySelector('.name3').addEventListener('mouseout', function() {
+  document.querySelector('.icon3').style.color = "var(--highlight_color)";
+
+});
+
+
+
+
+
+
